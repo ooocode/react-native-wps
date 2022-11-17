12b9129daec6c55f6d3ede36463d3c680ec3c08c
SmithTom <943866961@qq.com>
2022-11-17 20:34:16 +0800

    chore: initial commit

---
+++ android/src/main/java/com/wps/WpsModule.java
@@ -0,0 +1,32 @@
+package com.wps;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = WpsModule.NAME)
+public class WpsModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "Wps";
+
+  public WpsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  WpsExample
+//
+
+import Foundation
